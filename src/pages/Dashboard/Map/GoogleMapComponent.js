86a8acc5f8223daa9d94648c81008032f26eb367
Olessia Potapova <opotapova@adfi.co>
2020-08-07 00:23:57 -0300

    removed google map api key

--- src/pages/Dashboard/Map/GoogleMapComponent.js
+++ src/pages/Dashboard/Map/GoogleMapComponent.js
@@ -77,7 +77,6 @@ const GoogleMapComponent = () => {
   return (
     <div className="map-side">
         <WrappedMap 
-          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDsnwxmsrbpTMeTrJaYMo3VcaWMLJv16lE`}
           loadingElement={<div style={{height:'100%'}}/>}
           containerElement={<div style={{height:'100%'}}/>}
           mapElement={<div style={{height:'100%'}}/>}
